3fc8ca82f4da39f4f8da4e3e7304163019c7deaa
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2023-10-02 15:08:13 +0300

    Projects introduced

---
+++ CSharp/Advanced/03-Sets-and-Dictionaries/Exercise/SetsAndDictionariesExercise/09.SoftUniExamResults/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _09.SoftUniExamResults
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> languageSubmissionsPair = new();
+
+            Dictionary<string, int> studentPointsPair = new();
+
+            string input = Console.ReadLine();
+
+            while (input != "exam finished")
+            {
+                string[] contents = input.Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+                string student = contents[0];
+
+                if (contents[1] == "banned" && studentPointsPair.ContainsKey(student))
+                {
+                    studentPointsPair.Remove(student);
+                }
+                else
+                {
+                    string language = contents[1];
+                    int points = int.Parse(contents[2]);
+
+                    if (!studentPointsPair.ContainsKey(student))
+                    {
+                        studentPointsPair.Add(student, points);
+                    }
+                    else if (points > studentPointsPair[student])
+                    {
+                        studentPointsPair[student] = points;
+                    }
+
+                    if (languageSubmissionsPair.ContainsKey(language))
+                    {
+                        languageSubmissionsPair[language]++;
+                    }
+                    else
+                    {
+                        languageSubmissionsPair.Add(language, 1);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Results:");
+
+            foreach (var (student, points) in studentPointsPair.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{student} | {points}");
+            }
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var (language, submissions) in languageSubmissionsPair.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{language} - {submissions}");
+            }
+        }
+    }
+}
---
+++ CSharp/Advanced/03-Sets-and-Dictionaries/Exercise/SetsAndDictionariesExercise/10.ForceBook/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _10.ForceBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> sideUserPair = new();
+
+            string command = Console.ReadLine();
+
+            while (command != "Lumpawaroo")
+            {
+                if (command.Contains("|"))
+                {
+                    string[] commandParts = command.Split(" | ");
+                    string side = commandParts[0];
+                    string user = commandParts[1];
+
+                    if (sideUserPair.Any(x => x.Value.Contains(user)))
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+
+                    if (!sideUserPair.ContainsKey(side))
+                    {
+                        sideUserPair.Add(side, new List<string>());
+                    }
+
+                    if (!sideUserPair[side].Contains(user))
+                    {
+                        sideUserPair[side].Add(user);
+                    }
+                }
+                else if (command.Contains("->"))
+                {
+                    string[] commandParts = command.Split(" -> ");
+                    string user = commandParts[0];
+                    string side = commandParts[1];
+
+                    if (sideUserPair.Any(x => x.Value.Contains(user)))
+                    {
+                        foreach (var kvp in sideUserPair.Where(x => x.Value.Contains(user)))
+                        {
+                            kvp.Value.Remove(user);
+                        }
+                    }
+
+                    if (!sideUserPair.ContainsKey(side))
+                    {
+                        sideUserPair.Add(side, new List<string>());
+                    }
+
+                    sideUserPair[side].Add(user);
+
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var (side, users) in sideUserPair.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (users.Count > 0)
+                {
+                    Console.WriteLine($"Side: {side}, Members: {users.Count}");
+
+                    foreach (var user in users.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
