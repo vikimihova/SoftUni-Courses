51c027e199fb3a57b6ce73381f73c3341e0a66f5
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2023-10-07 17:25:41 +0300

    Source code introduced to project

--- CSharp/Advanced/05-Functional-Programming/Lab/FunctionalProgrammingLab/04.AddVAT/Program.cs
+++ CSharp/Advanced/05-Functional-Programming/Lab/FunctionalProgrammingLab/04.AddVAT/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Func<string, double> parser = x => double.Parse(x);
+
+            List<double> originalPrices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(parser).ToList();
+
+            Func<double, double> addVAT = x => x * 1.2;
+
+            List<double> newPrices = CalculateVATPrices(originalPrices, addVAT);
+
+            Action<double> print = x => Console.WriteLine($"{x:f2}");
+
+            newPrices.ForEach(print);
+
+        }
+
+        static List<double> CalculateVATPrices(List<double> prices, Func<double, double> addVAT)
+        {
+            for (int i = 0; i < prices.Count; i++)
+            {
+                prices[i] = addVAT(prices[i]);
+            }
+
+            return prices;
         }
     }
 }
--- CSharp/Advanced/05-Functional-Programming/Lab/FunctionalProgrammingLab/05.FilterByAge/Program.cs
+++ CSharp/Advanced/05-Functional-Programming/Lab/FunctionalProgrammingLab/05.FilterByAge/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace _05.FilterByAge
+﻿using System.Globalization;
+
+namespace _05.FilterByAge
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> personAge = new();
+
+            int numberOfEntries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                if (!personAge.ContainsKey(name))
+                {
+                    personAge.Add(name, age);
+                }
+            }
+
+            string inputCondition = Console.ReadLine();
+            int threshold = int.Parse(Console.ReadLine());
+            string inputFormat = Console.ReadLine();
+
+            Func<int, bool> condition = DefineCondition(inputCondition, threshold);
+
+            foreach (var (name, age) in personAge.Where(x => condition(x.Value)))
+            {
+                Console.WriteLine(DefineFormat(inputFormat, name, age));
+            }
+        }
+
+        static Func<int, bool> DefineCondition(string inputCondition, int threshold)
+        {
+            Func<int, bool> condition = null;
+
+            if (inputCondition == "younger")
+            {
+                condition = x => x < threshold;
+            }
+            else if (inputCondition == "older")
+            {
+                condition = x => x >= threshold;
+            }
+
+            return condition;
+        }
+
+        static string DefineFormat(string inputFormat, string name, int age)
+        {
+            string format = string.Empty;
+
+            if (inputFormat == "name age")
+            {
+                format = name + " - " + age;
+            }
+            else if (inputFormat == "name")
+            {
+                format = name;
+            }
+            else if (inputFormat == "age")
+            {
+                format = age.ToString();
+            }
+
+            return format;
         }
     }
 }
