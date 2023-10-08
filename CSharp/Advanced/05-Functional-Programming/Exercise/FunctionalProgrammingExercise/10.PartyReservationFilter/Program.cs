5e3e48b46abdd64c25cc0d8550e26ad651ef975d
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2023-10-08 22:30:39 +0300

    Projects introduced

---
+++ CSharp/Advanced/05-Functional-Programming/Exercise/FunctionalProgrammingExercise/10.PartyReservationFilter/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _10.PartyReservationFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .ToList();
+
+            string input = Console.ReadLine();
+
+            Dictionary<string, Predicate<string>> filters = new();
+
+            while (input != "Print")
+            {
+                string[] commandTokens = input.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = commandTokens[0];
+                string filter = commandTokens[1];
+                string value = commandTokens[2];
+
+                Predicate<string> predicate = DefinePredicate(filter, value);
+
+                if (action.Contains("Add"))
+                {
+                    filters.Add(filter + value, predicate);
+                }
+                else if (action.Contains("Remove"))
+                {
+                    filters.Remove(filter + value);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var kvp in filters)
+            {
+                guests.RemoveAll(kvp.Value);
+            }
+
+            Console.WriteLine(string.Join(" ", guests));
+        }
+
+        static Predicate<string> DefinePredicate(string filter, string value)
+        {
+            if (filter == "Starts with")
+            {
+                return x => x.StartsWith(value);
+            }
+            else if (filter == "Ends with")
+            {
+                return x => x.EndsWith(value);
+            }
+            else if (filter == "Length")
+            {
+                return x => x.Length == int.Parse(value);
+            }
+            else if (filter == "Contains")
+            {
+                return x => x.Contains(value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CSharp/Advanced/05-Functional-Programming/Exercise/FunctionalProgrammingExercise/11.TriFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minSum = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .ToList();
+
+            Func<string, int, bool> condition = (name, minSum) => name.Sum(x => x) >= minSum;
+
+
+            Func<int, List<string>, Func<string, int, bool>, string> generateResult =
+                (minSum, names, condition) => names.FirstOrDefault(name => condition(name, minSum));
+
+            Console.WriteLine(generateResult(minSum, names, condition));
+        }
+    }
+}
