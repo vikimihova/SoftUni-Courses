5fc75e673a29effa676115f0bfab790d62ce3256
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2023-10-11 23:42:23 +0300

    Project introduced

--- CSharp/Advanced/06-Defining-Classes/Exercise/09.PokemonTrainer/StartUp.cs
+++ CSharp/Advanced/06-Defining-Classes/Exercise/09.PokemonTrainer/StartUp.cs
@@ -1,10 +1,67 @@
-﻿namespace _09.PokemonTrainer
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _09.PokemonTrainer
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Trainer> trainers = new();
+            HashSet<string> trainerNames = new();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+                string[] commandTokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = commandTokens[0];
+                string pokemonName = commandTokens[1];
+                string pokemonElement = commandTokens[2];
+                int pokemonHealth = int.Parse(commandTokens[3]);
+
+                Trainer trainer = new Trainer(trainerName);
+
+                if (!trainerNames.Contains(trainerName))
+                {
+                    trainers.Add(trainer);
+                }
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                trainer.Pokemons.Add(pokemon);
+            }
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                foreach (Trainer trainer in trainers)
+                {
+                    if (trainer.Pokemons.Any(x => x.Element == command))
+                    {
+                        trainer.BadgesCount++;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < trainer.Pokemons.Count; i++)
+                        {
+                            trainer.Pokemons[i].Health -= 10;
+
+                            if (trainer.Pokemons[i].Health <= 0)
+                            {
+                                trainer.Pokemons.Remove(trainer.Pokemons[i]);
+                                i--;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (Trainer trainer in trainers.OrderByDescending(x => x.BadgesCount))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.BadgesCount} {trainer.Pokemons.Count}");
+            }
         }
     }
 }
--- CSharp/Advanced/06-Defining-Classes/Exercise/09.PokemonTrainer/Trainer.cs
+++ CSharp/Advanced/06-Defining-Classes/Exercise/09.PokemonTrainer/Trainer.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PokemonTrainer
+namespace _09.PokemonTrainer
 {
     public class Trainer
     {
-    }
+        public Trainer(string name)
+        {
+            BadgesCount = 0;
+			Pokemons = new List<Pokemon>();
+        }
+
+        private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int badgesCount;
+
+		public int BadgesCount
+		{
+			get { return badgesCount; }
+			set { badgesCount = value; }
+		}
+
+		private List<Pokemon> pokemons;
+
+		public List<Pokemon> Pokemons
+		{
+			get { return pokemons; }
+			set { pokemons = value; }
+		}
+	}
 }
