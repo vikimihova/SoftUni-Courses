766b6bb15c95660dfcae72f2c493c6d37332d31e
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2023-11-19 22:24:38 +0200

    Solution and projects introduced

---
+++ CSharp/OOP/01-Inheritance/Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        //public RandomList(IEnumerable<string> collection) : base(collection)
+        //{
+        //}
+
+        public string RandomString()
+        {
+            Random random = new Random();
+
+            string randomElement = this[random.Next(0, this.Count)];
+
+            this.Remove(randomElement);
+
+            return randomElement;
+        }
+    }
+}
---
+++ CSharp/OOP/01-Inheritance/Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //string[] array = new string[] { "fsg", "fsgg", "sghh"};
+
+            RandomList newList = new RandomList();
+
+            newList.Add("sfdgx");
+            newList.Add("Garlick");
+            newList.Add("sfsff");
+
+            Console.WriteLine(string.Join("\n", newList));
+
+            newList.RandomString();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join("\n", newList));
+
+            newList.RandomString();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join("\n", newList));
+
+            newList.RandomString();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join("\n", newList));
+        }
+    }
+}
