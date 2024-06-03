8fc23018ba841dae84829b4810063db6dc49b610
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-06-03 11:08:26 +0300

    Query solved
    Error corrected, ordering and final syntax

--- MSSQL/05-Subqueries-And-Joins/09-EmployeeManager.sql
+++ MSSQL/05-Subqueries-And-Joins/09-EmployeeManager.sql
@@ -1,9 +1,10 @@
 --09.Employee manager
 
-SELECT [e].EmployeeID,
-	   [e].[FirstName],
-	   [e].[ManagerID],
-	   [m].[FirstName]
-FROM [Employees] AS [e]
-JOIN [Employees] AS [m] ON [e].ManagerID = [m].[ManagerID]
-WHERE [m].[ManagerID] IN (3, 7)
+  SELECT [e].[EmployeeID],
+	     [e].[FirstName],
+	     [e].[ManagerID],
+	     [m].[FirstName] AS [ManagerName]
+	FROM [Employees] AS [e]
+	JOIN [Employees] AS [m] ON [e].[ManagerID] = [m].[EmployeeID]
+   WHERE [m].[EmployeeID] IN (3, 7)
+ORDER BY [e].[EmployeeID]
