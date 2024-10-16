162ff7950e26d1234329648b104638aabebd9f26
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-10-16 15:59:51 +0300

    Test solution

---
+++ CSharp/ASP.NET-Fundamentals/Test/CinemaApp/CinemaApp.Data/CinemaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CinemaApp.Data.Models;
+using System.Reflection;
+
+namespace CinemaApp.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext()
+        {            
+        }
+
+        public CinemaDbContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        public virtual DbSet<Cinema> Cinemas { get; set; }
+
+        public virtual DbSet<CinemaMovie> CinemasMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
