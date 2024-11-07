ceb3ee7c6c64d67c216b44f6b4ba15b96c41e357
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-07 22:19:32 +0200

    build(services): add manager service

---
+++ CSharp/Web/ASP.NET-Advanced/Workshop/CinemaApp/CinemaApp.Services.Data/ManagerService.cs
@@ -0,0 +1,32 @@
+﻿using CinemaApp.Data.Models;
+using CinemaApp.Data.Repository.Interfaces;
+using CinemaApp.Services.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Services.Data
+{
+    public class ManagerService : BaseService, IManagerService
+    {
+        private readonly IRepository<Manager, Guid> managersRepository;
+
+        public ManagerService(IRepository<Manager, Guid> managersRepository)
+        {
+            this.managersRepository = managersRepository;
+        }
+
+        public async Task<bool> IsUserManagerAsync(string? userId)
+        {
+            // Unlikely, but defensive programming is a good practice
+            if (String.IsNullOrWhiteSpace(userId))
+            {
+                return false;
+            }
+
+            bool result = await this.managersRepository
+                .GetAllAttached()
+                .AnyAsync(m => m.UserId.ToString().ToLower() == userId);
+
+            return result;
+        }
+    }
+}
---
+++ CSharp/Web/ASP.NET-Advanced/Workshop/CinemaApp/CinemaApp.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace CinemaApp.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal? userClaimsPrincipal)
+        {
+            return userClaimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null;
+            // Claim.Types.NameIdentifier returns the current user's id
+        }
+    }
+}
