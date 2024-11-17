c350f8fed9f77118e0b84c92658479824d3bf6f1
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2024-11-17 17:38:26 +0200

    build(ticket): add view models

---
+++ CSharp/Web/ASP.NET-Advanced/Workshop/CinemaApp/CinemaApp.Web.ViewModels/Ticket/BuyTicketViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CinemaApp.Services.Mapping;
+using System.ComponentModel.DataAnnotations;
+
+using static CinemaApp.Common.EntityValidationConstants.Ticket;
+using static CinemaApp.Common.EntityValidationMessages.Ticket;
+
+namespace CinemaApp.Web.ViewModels.Ticket
+{
+    public class BuyTicketViewModel : IMapTo<Data.Models.Ticket>
+    {
+        [Required]
+        public string CinemaId { get; set; } = null!;
+
+        [Required]
+        public string MovieId { get; set; } = null!;
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [Range(typeof(decimal), PriceMinValue, PriceMaxValue, ErrorMessage = IncorrectPriceMessage)]
+        public decimal Price { get; set; }
+
+        [Range(CountMinValue, CountMaxValue, ErrorMessage = IncorrectCountMessage)]
+        public int Count { get; set; }
+    }
+}
---
+++ CSharp/Web/ASP.NET-Advanced/Workshop/CinemaApp/CinemaApp.Web.ViewModels/Ticket/UserTicketViewModel.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CinemaApp.Services.Mapping;
+
+namespace CinemaApp.Web.ViewModels.Ticket
+{
+    public class UserTicketViewModel : IMapFrom<Data.Models.Ticket>, IHaveCustomMappings
+    {
+        public string Id { get; set; } = null!;
+
+        public string MovieTitle { get; set; } = null!;
+
+        public string CinemaName { get; set; } = null!;
+
+        public string CinemaLocation { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<Data.Models.Ticket, UserTicketViewModel>()
+                .ForMember(d => d.MovieTitle,
+                    opt => opt.MapFrom(s => s.Movie.Title))
+                .ForMember(d => d.CinemaName, opt => opt.MapFrom(s => s.Cinema.Name))
+                .ForMember(d => d.CinemaLocation, opt => opt.MapFrom(s => s.Cinema.Location))
+                .ForMember(d => d.Price, opt => opt.MapFrom(s => s.Price.ToString("F2")));
+        }
+    }
+}
