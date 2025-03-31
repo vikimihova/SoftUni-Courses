3a5ed79cd01a02e872fcca138923ce6842cefcb9
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-03-31 13:10:51 +0300

    introduce solution for simple sorting of three numbers

---
+++ CSharp/Fundamentals/01-BasicSyntax/More Exercises/01-Fundamentals-Basic-Syntax-MoreExercises/01-SortNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int tempMax = Math.Max(a, b);
+int tempMin = Math.Min(a, b);
+
+int maxNumber = Math.Max(tempMax, c);
+int minNumber = Math.Min(tempMin, c);
+int midNumber;
+
+if (maxNumber == c)
+{
+    midNumber = tempMax;
+}
+else
+{
+    midNumber = Math.Max(tempMin, c);
+}
+
+Console.WriteLine(maxNumber);
+Console.WriteLine(midNumber);
+Console.WriteLine(minNumber);
+
+
+ 
