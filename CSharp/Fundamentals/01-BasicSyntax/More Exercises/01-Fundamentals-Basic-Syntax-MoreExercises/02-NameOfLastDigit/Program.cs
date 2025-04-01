628b45d652f530611f502f92b748cf6d46ddcb3a
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-01 19:06:32 +0300

    introduce console app that gets the name of the last digit of a number

---
+++ CSharp/Fundamentals/01-BasicSyntax/More Exercises/01-Fundamentals-Basic-Syntax-MoreExercises/02-NameOfLastDigit/Program.cs
@@ -0,0 +1,46 @@
+﻿int fullNumber = int.Parse(Console.ReadLine());
+
+int lastDigit = fullNumber % 10;
+
+Console.WriteLine(GetDigitName(lastDigit));
+
+static string GetDigitName(int digit)
+{
+    string name = String.Empty;
+
+    switch (digit)
+    {
+        case 0:
+            name = "zero";
+            break;
+        case 1:
+            name = "one";
+            break;
+        case 2:
+            name = "two";
+            break;
+        case 3:
+            name = "three";
+            break;
+        case 4:
+            name = "four";
+            break;
+        case 5:
+            name = "five";
+            break;
+        case 6:
+            name = "six";
+            break;
+        case 7:
+            name = "seven";
+            break;
+        case 8:
+            name = "eight";
+            break;
+        case 9:
+            name = "nine";
+            break;
+    }
+
+    return name;
+};
