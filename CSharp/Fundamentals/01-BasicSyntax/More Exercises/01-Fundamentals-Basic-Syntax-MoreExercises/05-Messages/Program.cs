8f1c1cc196aba49fe136899dd5e37d75304e1fd8
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-02 17:06:31 +0300

    introduce console app for mobile phone keyboard messages

---
+++ CSharp/Fundamentals/01-BasicSyntax/More Exercises/01-Fundamentals-Basic-Syntax-MoreExercises/05-Messages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+StringBuilder message = new StringBuilder();
+string input;
+
+while ((input = Console.ReadLine()) != String.Empty)
+{
+    int digit = int.Parse(input) % 10;
+
+    if (digit == 0)
+    {
+        message.Append(' ');
+        continue;
+    }
+
+    if (digit == 1)
+    {
+        continue;
+    }
+
+    int digitCount = input.Length;
+    int indexOffset = 0;
+
+    if (digit >= 2 && digit <= 7)
+    {
+        indexOffset = (digit - 2) * 3;
+    }
+    else if (digit >= 8)
+    {
+        indexOffset = (digit - 2) * 3 + 1;
+    }
+
+    int index = indexOffset + digitCount - 1;
+
+    message.Append((char)(97 + index));
+}
+
+Console.WriteLine(message.ToString());
