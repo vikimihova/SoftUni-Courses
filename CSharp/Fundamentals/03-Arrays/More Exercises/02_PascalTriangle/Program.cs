5116f0c9c168e1f9c52099a1da1ee0f89062f98e
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-03 19:03:34 +0300

    introduce console app for Pascal Triangle without using a matrix

---
+++ CSharp/Fundamentals/03-Arrays/More Exercises/02_PascalTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿int numberOfRows = int.Parse(Console.ReadLine());
+
+long[] lastRow = new long[numberOfRows];
+lastRow[0] = 1;
+
+for (int i = 1; i <= numberOfRows; i++)
+{
+    long[] currentRow = new long[i];
+    currentRow[0] = 1;
+    currentRow[i - 1] = 1;
+
+    for (int j = 1; j < i - 1; j++)
+    {
+        currentRow[j] = lastRow[j] + lastRow[j - 1];
+    }
+
+    Console.WriteLine(string.Join(" ", currentRow));
+
+    for (int k = 0; k < i; k++)
+    {
+        lastRow[k] = currentRow[k];
+    }
+}
