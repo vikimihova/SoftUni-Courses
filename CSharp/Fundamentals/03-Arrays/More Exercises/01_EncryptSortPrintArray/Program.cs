5de244c97e45053379cffd458e21d071273f140c
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-03 18:16:40 +0300

    introduce console app for encrypting and sorting an array of strings

---
+++ CSharp/Fundamentals/03-Arrays/More Exercises/01_EncryptSortPrintArray/Program.cs
@@ -0,0 +1,35 @@
+﻿int numberOfStrings = int.Parse(Console.ReadLine());
+
+int[] encryptedValues = new int[numberOfStrings]; 
+
+for (int i = 0; i < numberOfStrings; i++)
+{
+    string input = Console.ReadLine();
+
+    int length = input.Length;
+    int encryption = 0;
+
+    for (int j = 0; j < length; j++)
+    {
+        char letter = input[j];
+        bool isVowel = "AEIOUaeiou".IndexOf(letter) >= 0;
+
+        if (isVowel)
+        {
+            int vowelCode = letter * length;
+            encryption += vowelCode;
+        }
+        else
+        {
+            int consonantCode = letter / length;
+            encryption += consonantCode;
+        }
+    }
+
+    encryptedValues[i] = encryption;
+}
+
+foreach (int encryptedValue in encryptedValues.Order())
+{
+    Console.WriteLine(encryptedValue);
+}
