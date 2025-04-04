ac45494ec05fe85f175dee967616801539ba60d3
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-04 14:27:41 +0300

    introduce console app for nth Fibonacci number with recursion

---
+++ CSharp/Fundamentals/03-Arrays/More Exercises/03_RecursiveFibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿int nthFibonacciPosition = int.Parse(Console.ReadLine());
+int nthFibonacciNumber = GetFibonacci(nthFibonacciPosition);
+
+Console.WriteLine(nthFibonacciNumber);
+
+static int GetFibonacci(int n)
+{
+    if (n == 2 || n == 1)
+    {
+        return 1;
+    }
+
+    return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+}
