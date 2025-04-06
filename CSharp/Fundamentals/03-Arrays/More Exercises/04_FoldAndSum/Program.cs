3ae59fa8f2720afa48efd0c0a216d7acce368a1e
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-06 17:02:02 +0300

    introduce console app for folding an array

---
+++ CSharp/Fundamentals/03-Arrays/More Exercises/04_FoldAndSum/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] array = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rowLength = array.Length / 2;
+int numbersToFold = array.Length / 4;
+
+int[] topRow = new int[rowLength];
+int[] bottomRow = new int[rowLength];
+int[] resultRow = new int[rowLength];
+
+for (int i = 0; i < rowLength; i++)
+{
+    int stayIndex = numbersToFold + i;
+    int foldIndex = numbersToFold - i - 1;
+
+    if (foldIndex < 0)
+    {
+        foldIndex += array.Length;
+    }
+
+    topRow[i] = array[foldIndex];
+    bottomRow[i] = array[stayIndex];
+
+    resultRow[i] = topRow[i] + bottomRow[i];
+}
+
+Console.WriteLine(string.Join(" ", resultRow));
