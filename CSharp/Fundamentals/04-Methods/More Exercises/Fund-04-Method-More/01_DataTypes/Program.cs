b7296aefa89b7fca3e5ceab80fb7e5d756a3009d
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-14 18:26:41 +0300

    introduce console app for method overloading

---
+++ CSharp/Fundamentals/04-Methods/More Exercises/Fund-04-Method-More/01_DataTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _01_DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string value = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int number = int.Parse(value);
+                Console.WriteLine(GetResult(number));
+            }
+
+            if (type == "real")
+            {
+                double realNumber = double.Parse(value);
+                Console.WriteLine(GetResult(realNumber));
+
+            }
+
+            if (type == "string")
+            {
+                Console.WriteLine(GetResult(value));
+            }      
+        }
+
+        static string GetResult(int number)
+        {
+            int result = number * 2;
+            return result.ToString();
+        }
+
+        static string GetResult(double realNumber)
+        {
+            double result = realNumber * 1.5;
+            return result.ToString("F2");
+        }
+
+        static string GetResult(string text)
+        {
+            return "$" + text + "$";
+        }
+    }
+}
