e001bd516bf6521598711833a19573fb5072972d
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-16 23:55:32 +0300

    refactor: method signatures changed

--- CSharp/Fundamentals/04-Methods/More Exercises/Fund-04-Method-More/02_CenterPoint/Program.cs
+++ CSharp/Fundamentals/04-Methods/More Exercises/Fund-04-Method-More/02_CenterPoint/Program.cs
@@ -11,22 +11,26 @@
             // read the coordinates of the second point
             int x2 = int.Parse(Console.ReadLine());
             int y2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(GetClosestCoordinatesToCenter(x1, y1, x2, y2));
+            PrintClosestPointToCenter(point1, point2);
         }
 
-        static string GetClosestCoordinatesToCenter(int x1, int y1, int x2, int y2)
+        }
+
+        static void PrintClosestPointToCenter(int[] point1, int[] point2)
         {
+            (int x1, int y1) = (point1[0], point1[1]);
+            (int x2, int y2) = (point2[0], point2[1]);
+
             int absoluteDistance1 = Math.Abs(x1 + y1);
             int absoluteDistance2 = Math.Abs(x2 + y2);
 
             if (absoluteDistance1 <= absoluteDistance2)
             {
-                return $"({x1}, {y1})";
+                Console.WriteLine($"({x1}, {y1})");
             }
             else
             {
-                return $"({x2}, {y2})";
+                Console.WriteLine($"({x2}, {y2})");
             }
         }
     }
