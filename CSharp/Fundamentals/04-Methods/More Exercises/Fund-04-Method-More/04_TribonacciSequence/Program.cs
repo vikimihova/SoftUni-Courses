bc237e31589dc1441d30227db436f8a46add22fd
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-17 23:00:51 +0300

    introduce console app for printing a Tribonacci sequence

---
+++ CSharp/Fundamentals/04-Methods/More Exercises/Fund-04-Method-More/04_TribonacciSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _04_TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceCount = int.Parse(Console.ReadLine());
+
+            if (sequenceCount <= 0)
+            {
+                return;
+            }
+
+            int[] tribonacciSequence = new int[sequenceCount];
+            tribonacciSequence[0] = 1;
+
+            for (int i = 1; i < sequenceCount; i++)
+            {
+                tribonacciSequence[i] = CalculateTribonacciNumber(i, tribonacciSequence);
+            }
+
+            Console.WriteLine(string.Join(' ', tribonacciSequence));
+        }
+
+        static int CalculateTribonacciNumber(int index, int[] sequence)
+        {
+            int number = 0;
+            int i = index - 1;
+
+            while (i >= 0 && i >= index - 3)
+            {
+                number += sequence[i--];
+            }
+
+            return number;
+        }
+    }
+}
