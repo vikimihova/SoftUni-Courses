efd4e9c309b13081ad01d023151fc7b285d16b04
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-18 16:06:12 +0300

    introduce console app for determining the product sign of a multiplication

---
+++ CSharp/Fundamentals/04-Methods/More Exercises/Fund-04-Method-More/05_MultiplicationSign/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _05_MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            string productSign = DetermineProductSign(numbers);
+
+            Console.WriteLine(productSign);
+        }
+
+        static string DetermineProductSign(int[] numbers)
+        {
+            int negativeSignCount = 0;
+            bool isZero = false;
+
+            foreach (int number in numbers)
+            {
+                if (number < 0)
+                {
+                    negativeSignCount++;
+                }
+
+                if (number == 0)
+                {
+                    isZero = true;
+                    break;
+                }
+            }
+
+            if (isZero) 
+            {
+                return "zero";
+            }
+            else if (negativeSignCount % 2 == 0)
+            {
+                return "positive";
+            }
+            else
+            {
+                return "negative";
+            }
+        }
+    }
+}
